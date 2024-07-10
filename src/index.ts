a722062b9a33b84f2392b5ca1b2e6b567e983e04
Adityagupta1625 <guptaaditya2512@gmail.com>
2024-07-10 09:57:42 +0530

    fix: cors config added and port as well

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ app.use(cors({
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const PORT = 8000
+const PORT =  process.env.PORT || 8000
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello world')
